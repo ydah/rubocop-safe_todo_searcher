4eae67b81de53aa203d7e86da41fee9efd479e6f
ydah <13041216+ydah@users.noreply.github.com>
2021-12-27 18:46:56 +0900

    Initial commit

---
+++ lib/rubocop/safe_todo_searcher.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "safe_todo_searcher/version"
+
+module Rubocop
+  module SafeTodoSearcher
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rubocop/safe_todo_searcher_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubocop::SafeTodoSearcher do
+  it "has a version number" do
+    expect(Rubocop::SafeTodoSearcher::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
