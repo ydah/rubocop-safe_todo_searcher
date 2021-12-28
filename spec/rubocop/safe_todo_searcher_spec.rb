6b77d35094e44723c41caa4f15e16f40d27e14df
ydah <13041216+ydah@users.noreply.github.com>
2021-12-29 01:26:59 +0900

    Remove failed sample test

--- spec/rubocop/safe_todo_searcher_spec.rb
+++ spec/rubocop/safe_todo_searcher_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rubocop::SafeTodoSearcher do
   it "has a version number" do
     expect(Rubocop::SafeTodoSearcher::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
