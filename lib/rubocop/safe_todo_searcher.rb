7005c9c9165aefccd67f7d2c912593f87ee6b229
ydah <13041216+ydah@users.noreply.github.com>
2022-01-02 08:16:10 +0900

    Fix to not search `rubocop_todo.yml` if it doesn't exist

--- lib/rubocop/safe_todo_searcher.rb
+++ lib/rubocop/safe_todo_searcher.rb
@@ -8,14 +8,16 @@ module Rubocop
     class Error < StandardError; end
 
     def self.search
-      res = ""
-      open(".rubocop_todo.yml", "r") { |f| YAML.safe_load(f) }.each_key do |key|
-        klass = Object.const_get "RuboCop::Cop::#{key.gsub(%r{/}, "::")}"
-        res << "#{key}\n" if klass.support_autocorrect?
-      rescue StandardError
-        nil
+      if File.exist?(".rubocop_todo.yml")
+        res = ""
+        open(".rubocop_todo.yml", "r") { |f| YAML.safe_load(f) }.each_key do |key|
+          klass = Object.const_get "RuboCop::Cop::#{key.gsub(%r{/}, "::")}"
+          res << "#{key}\n" if klass.support_autocorrect?
+        rescue StandardError
+          nil
+        end
+        res
       end
-      res
     end
   end
 end
