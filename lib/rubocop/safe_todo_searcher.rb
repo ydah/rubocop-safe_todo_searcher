291cbcb8e8c8ee37661860c1a4082b1c6e15c897
ydah <13041216+ydah@users.noreply.github.com>
2022-01-02 08:51:37 +0900

    Create method self.support_autocorrect?

--- lib/rubocop/safe_todo_searcher.rb
+++ lib/rubocop/safe_todo_searcher.rb
@@ -11,8 +11,7 @@ module Rubocop
       if File.exist?(".rubocop_todo.yml")
         res = ""
         open(".rubocop_todo.yml", "r") { |f| YAML.safe_load(f) }.each_key do |key|
-          klass = Object.const_get "RuboCop::Cop::#{key.gsub(%r{/}, "::")}"
-          res << "#{key}\n" if klass.support_autocorrect?
+          res << "#{key}\n" if support_autocorrect?(key)
         rescue StandardError
           nil
         end
@@ -20,6 +19,11 @@ module Rubocop
       else
         "rubocop_todo.yml does not exist"
       end
+    end
+
+    def self.support_autocorrect?(key)
+      klass = Object.const_get "RuboCop::Cop::#{key.gsub(%r{/}, "::")}"
+      klass.support_autocorrect?
     end
   end
 end
