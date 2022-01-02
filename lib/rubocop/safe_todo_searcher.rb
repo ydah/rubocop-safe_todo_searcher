b1aeac4f130150c4fe984b9625c083424a82d8fe
ydah <13041216+ydah@users.noreply.github.com>
2022-01-02 09:10:46 +0900

    Change the result to an instance variable

--- lib/rubocop/safe_todo_searcher.rb
+++ lib/rubocop/safe_todo_searcher.rb
@@ -7,15 +7,16 @@ module Rubocop
   module SafeTodoSearcher
     class Error < StandardError; end
 
+    @result = ""
+
     def self.search
       if File.exist?(".rubocop_todo.yml")
-        res = ""
         open(".rubocop_todo.yml", "r") { |f| YAML.safe_load(f) }.each_key do |key|
-          res << "#{key}\n" if support_autocorrect?(key)
+          @result << "#{key}\n" if support_autocorrect?(key)
         rescue StandardError
           nil
         end
-        res
+        @result
       else
         "rubocop_todo.yml does not exist"
       end
