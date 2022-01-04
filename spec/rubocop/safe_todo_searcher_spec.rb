1ab3f3f648aa7b696200e1c884adb1e0e09b6d3d
ydah <13041216+ydah@users.noreply.github.com>
2022-01-04 09:24:10 +0900

    Add a block of version-defined tests

--- spec/rubocop/safe_todo_searcher_spec.rb
+++ spec/rubocop/safe_todo_searcher_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe Rubocop::SafeTodoSearcher do
-  it "has a version number" do
-    expect(Rubocop::SafeTodoSearcher::VERSION).not_to be nil
+  describe "version" do
+    it "has a version number" do
+      expect(Rubocop::SafeTodoSearcher::VERSION).not_to be nil
+    end
   end
 end
