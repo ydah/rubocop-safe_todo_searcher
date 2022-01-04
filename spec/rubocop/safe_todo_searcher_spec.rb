8041a88fac26561216220c59824910356da62258
ydah <13041216+ydah@users.noreply.github.com>
2022-01-04 09:46:33 +0900

    Add test to returns true when supported auto-correct

--- spec/rubocop/safe_todo_searcher_spec.rb
+++ spec/rubocop/safe_todo_searcher_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Rubocop::SafeTodoSearcher do
       expect(Rubocop::SafeTodoSearcher::VERSION).not_to be nil
     end
   end
+
+  describe "support_autocorrect?" do
+    context "auto-correct is supported" do
+      it "returns true" do
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Bundler/OrderedGems")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Gemspec/DateAssignment")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Layout/AccessModifierIndentation")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Lint/AmbiguousOperatorPrecedence")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Migration/DepartmentName")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Naming/BinaryOperatorParameterName")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Security/YAMLLoad")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Style/AccessorGrouping")).to be true
+        expect(Rubocop::SafeTodoSearcher.support_autocorrect?("Rails/ActionFilter")).to be true
+      end
+    end
+  end
 end
