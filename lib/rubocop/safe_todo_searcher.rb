6e1db83806b84fe35258346a3e887f1b786ce26f
ydah <13041216+ydah@users.noreply.github.com>
2022-02-19 01:32:45 +0900

    feat: to output the minimum required

--- lib/rubocop/safe_todo_searcher.rb
+++ lib/rubocop/safe_todo_searcher.rb
@@ -7,6 +7,8 @@ require "rubocop-rails"
 require "rubocop-rake"
 require "rubocop-rspec"
 require "rubocop-sequel"
+require "date"
+require "highline"
 require "yaml"
 require_relative "safe_todo_searcher/version"
 
@@ -14,10 +16,17 @@ module Rubocop
   module SafeTodoSearcher
     class << self
       def search
-        File.exist?(".rubocop_todo.yml") ? parse : "rubocop_todo.yml does not exist"
+        puts generate_header
+        puts horizontal_line
+        puts generate_results
+        puts horizontal_line
       end
 
       private
+
+      def generate_results
+        File.exist?(".rubocop_todo.yml") ? parse : "rubocop_todo.yml does not exist"
+      end
 
       def parse
         res = +""
@@ -32,6 +41,27 @@ module Rubocop
         cop.support_autocorrect? && cop.new(RuboCop::ConfigLoader.default_configuration).safe_autocorrect?
       rescue NameError
         false
+      end
+
+      def generate_header
+        [
+          horizontal_line,
+          header("Rubocop TODO searcher"),
+          title("Version", Rubocop::SafeTodoSearcher::VERSION),
+          title("Date", DateTime.now.strftime("%Y-%m-%d %H:%M:%S"))
+        ]
+      end
+
+      def header(text)
+        HighLine.new.color("++++++ #{text} ++++++", :bold)
+      end
+
+      def title(title, value, color = :green)
+        "#{HighLine.new.color(title, color)}: #{value}"
+      end
+
+      def horizontal_line(color = :none)
+        HighLine.new.color("--------------------------------------------------", :bold, color)
       end
     end
   end
