0b22f7f888d8ba81210704b4ddf37ec8d66a1bad
ydah <13041216+ydah@users.noreply.github.com>
2022-02-18 23:41:57 +0900

    refactor: .rubocop_todo.yml to be constant

--- lib/rubocop/safe_todo_searcher.rb
+++ lib/rubocop/safe_todo_searcher.rb
@@ -12,16 +12,18 @@ require_relative "safe_todo_searcher/version"
 
 module Rubocop
   module SafeTodoSearcher
+    RUBOCOP_TODO_YML = ".rubocop_todo.yml"
+
     class << self
       def search
-        File.exist?(".rubocop_todo.yml") ? parse : "rubocop_todo.yml does not exist"
+        File.exist?(RUBOCOP_TODO_YML) ? parse : "#{RUBOCOP_TODO_YML} does not exist"
       end
 
       private
 
       def parse
         res = +""
-        File.open(".rubocop_todo.yml", "r") { |f| YAML.safe_load(f) }&.each_key do |key|
+        File.open(RUBOCOP_TODO_YML, "r") { |f| YAML.safe_load(f) }&.each_key do |key|
           res << "#{key}\n" if support_autocorrect?(key)
         end
         res
